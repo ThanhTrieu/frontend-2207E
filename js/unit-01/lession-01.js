c6e3ca90a74809faf3d4912916a1d55ad5fef565
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2022-08-18 23:41:07 +0700

    update code

---
+++ js/unit-01/lession-01.js
@@ -0,0 +1,17 @@
+let hello = "Hello word";
+hello = 20;
+console.log(hello);
+// let : keyword khai bao bien (cu phap phai viet dung)
+// hello : ten bien (LTV tu dinh nghia)
+// "Hello word" || 20: gia tri cua bien (LTV tu khai bao)
+// 1 so kieu du lieu co ban
+// 1- kieu number
+let number = 10; // gan gia la 1 so(khon phan biet cac loai so)
+// kiem tra kieu du lieu cua bien
+console.log(typeof number);
+console.log(typeof(number));
+let number2 = number/0;
+console.log(typeof(number2), number2);
+let number3 = '20';
+let str = `sap dc nghi roi con ${num3} phut`;
+let sumNumber = number + number3;
